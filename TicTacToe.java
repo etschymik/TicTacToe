d689b0882773642cb3f5e99e4c1e4b60838f3c38
etschymik <70454939+etschymik@users.noreply.github.com>
2020-09-11 10:54:47 -0400

    updated the game to add singleplayer easy mode

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
     static Scanner s = new Scanner(System.in);
-
     static String[] board = new String[9];
     static String winner = null;
     static String turn = "X";
     static int input;
-
     static int xWins = 0;
     static int oWins = 0;
 
@@ -19,7 +18,55 @@ public class TicTacToe {
     // game modes
     //-----------------
     static void easyMode() {
+        assignSlots();
+        while(winner == null) {
+            // X's turn (player)
+            if(turn.equals("X")) {
+                userInput();
+                // checks if the number is valid
+                if(input < 1 || input > 9) {
+                    System.out.println("Invalid Input");
+                    System.out.println("Choose a number 1-9");
+                    updateBoard();
+                    continue;
+                }
+                // checks if X or O is already in spot
+                else if(board[input-1].equals("X") || board[input-1].equals("O")) {
+                    System.out.println("Spot already taken");
+                    System.out.println("Choose a number 1-9");
+                    updateBoard();
+                    continue;
+                }
+                // if the number is valid and spot not taken then place the character in array
+                else {
+                    board[input-1] = turn;
+                    changeTurn();
+                    winner = checkForWinner();
+                    updateBoard();
+                }
+            }
 
+            // O's turn (computer)
+            if(turn.equals("O")) {
+                while(turn.equals("O")) {
+                    Random rand = new Random();
+                    int randomSpot = rand.nextInt(9);
+                    if(board[randomSpot].equals("X") || board[randomSpot].equals("O")) {
+                        continue;
+                    } else {
+                        board[randomSpot] = turn;
+                        System.out.println("The computer played an 'O' at " + (randomSpot + 1));
+                        changeTurn();
+                        winner = checkForWinner();
+                        updateBoard();
+                    }
+                }
+
+            }
+
+            win();
+            tie();
+        }
     }
 
     static void hardMode() {
@@ -27,6 +74,9 @@ public class TicTacToe {
     }
 
     static void multiplayerMode() {
+        System.out.println();
+        System.out.println("Welcome to Multiplayer Tic-Tac-Toe");
+
         assignSlots();
         while(winner == null) {
             userInput();
@@ -69,16 +119,10 @@ public class TicTacToe {
         System.out.println("Do you want to play single or multiplayer?");
         System.out.print("Enter 1 or 2: ");
         gameType = s.nextLine();
-        switch(gameType) {
-            case "1":
-                introSingleplayer();
-                break;
-            case "2":
-                introMultiplayer();
-                break;
-            default:
-                System.out.println("Not a valid input");
-                break;
+        switch (gameType) {
+            case "1" -> introSingleplayer();
+            case "2" -> multiplayerMode();
+            default -> System.out.println("Not a valid input");
         }
     }
 
@@ -90,23 +134,11 @@ public class TicTacToe {
         System.out.println("Do you want to play easy or hard?");
         System.out.print("Enter E or H: ");
         gameMode = s.nextLine();
-        switch(gameMode) {
-            case "E": case "e":
-                easyMode();
-                break;
-            case "H": case "h":
-                hardMode();
-                break;
-            default:
-                System.out.println("Not a valid input");
-                break;
+        switch (gameMode) {
+            case "E", "e" -> easyMode();
+            case "H", "h" -> hardMode();
+            default -> System.out.println("Not a valid input");
         }
-    }
-
-    static void introMultiplayer() {
-        System.out.println();
-        System.out.println("Welcome to Multiplayer Tic-Tac-Toe");
-        multiplayerMode();
     }
 
     //-----------------
@@ -116,30 +148,22 @@ public class TicTacToe {
         String check = null;
         for (int i = 0; i < 8; i++) {
             switch (i) {
-                case 0: // top row
-                    check = board[0] + board[1] + board[2];
-                    break;
-                case 1: // middle row
-                    check = board[3] + board[4] + board[5];
-                    break;
-                case 2: // bottom row
-                    check = board[6] + board[7] + board[8];
-                    break;
-                case 3: // left column
-                    check = board[0] + board[3] + board[6];
-                    break;
-                case 4: // middle column
-                    check = board[1] + board[4] + board[7];
-                    break;
-                case 5: // right column
-                    check = board[2] + board[5] + board[8];
-                    break;
-                case 6: // TL to BR diagonal
-                    check = board[0] + board[4] + board[8];
-                    break;
-                case 7: // TR to BL diagonal
-                    check = board[2] + board[4] + board[6];
-                    break;
+                // top row
+                case 0 -> check = board[0] + board[1] + board[2];
+                // middle row
+                case 1 -> check = board[3] + board[4] + board[5];
+                // bottom row
+                case 2 -> check = board[6] + board[7] + board[8];
+                // left column
+                case 3 -> check = board[0] + board[3] + board[6];
+                // middle column
+                case 4 -> check = board[1] + board[4] + board[7];
+                // right column
+                case 5 -> check = board[2] + board[5] + board[8];
+                // TL to BR diagonal
+                case 6 -> check = board[0] + board[4] + board[8];
+                // TR to BL diagonal
+                case 7 -> check = board[2] + board[4] + board[6];
             }
 
             // checks if the case is either XXX or OOO
@@ -216,9 +240,13 @@ public class TicTacToe {
         if(play.equals("Y") || play.equals("y")) {
             changeTurn();
             winner = null;
-            multiplayerMode();
+            pickGameMode();
         } else if(play.equals("N") || play.equals("n")) {
             System.exit(0);
         }
+    }
+
+    static void pickGameMode() {
+        multiplayerMode();
     }
 }
